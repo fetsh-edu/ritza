29f7f930e5bb137595504fe3499080d9481ea4e5
fetsh-edu <fetsh.me@gmail.com>
2024-03-25 17:28:04 +0200

    Add more og:content to Head

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -11,41 +11,54 @@ export default (() => {
       fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description
     const { css, js } = externalResources
 
+    const pageTitle = cfg.pageTitle
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
     const iconPath = joinSegments(baseDir, "static/logo.svg")
-    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
-
+    const ogImage = fileData.frontmatter?.image ?? "static/og-image.png"
+    const canonicalURL = `https://${cfg.baseUrl}/${fileData.slug}`
+    const ogImagePath = `https://${cfg.baseUrl}/${ogImage}`
+    const ogImageAlt = fileData.frontmatter?.imageAlt as string
     return (
-      <head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
-          <>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
-          </>
-        )}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
-        <meta name="description" content={description} />
-        <meta name="generator" content="Quartz" />
-        {css.map((href) => (
-          <link key={href} href={(href+"?date="+(new Date()).toISOString())} rel="stylesheet" type="text/css" spa-preserve />
-        ))}
-        {js
-          .filter((resource) => resource.loadTime === "beforeDOMReady")
-          .map((res) => JSResourceToScriptElement(res, true))}
-      </head>
+        <head>
+            <title>{title}</title>
+            <meta charSet="utf-8"/>
+            {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
+                <>
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                    <link rel="stylesheet" href={googleFontHref(cfg.theme)}/>
+                </>
+            )}
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+            {/*Essential META Tags*/}
+            <meta property="og:title" content={title}/>
+            <meta property="og:type" content="article"/>
+            {cfg.baseUrl && <meta property="og:image" content={ogImagePath}/>}
+            {cfg.baseUrl && <meta property="og:url" content={canonicalURL}/>}
+            <meta name="twitter:card" content="summary_large_image"/>
+
+            {/*Non-Essential, But Recommended*/}
+            <meta property="og:description" content={description}/>
+            <meta property="og:site_name" content={pageTitle}/>
+            {ogImageAlt && <meta name="twitter:image:alt" content={ogImageAlt}/>}
+
+            <link rel="icon" href={iconPath}/>
+            <link rel="canonical" href={canonicalURL} />
+            <meta name="description" content={description}/>
+            <meta name="generator" content="Love, Obsidian and Quartz"/>
+                {css.map((href) => (
+                    <link key={href} href={(href + "?date=" + (new Date()).toISOString())} rel="stylesheet"
+                          type="text/css" spa-preserve/>
+                ))}
+                {js
+                    .filter((resource) => resource.loadTime === "beforeDOMReady")
+                    .map((res) => JSResourceToScriptElement(res, true))}
+        </head>
     )
   }
 
-  return Head
+    return Head
 }) satisfies QuartzComponentConstructor
