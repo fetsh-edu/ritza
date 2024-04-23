5278aa426be14b77d954a7b5ea210185057258e6
fetsh-edu <fetsh.me@gmail.com>
2024-04-23 20:08:11 +0300

    Add Images transforemer

--- quartz/plugins/transformers/ogimage.ts
+++ quartz/plugins/transformers/ogimage.ts
@@ -1,10 +1,31 @@
 import {QuartzTransformerPlugin} from "../types";
+import {Root as HTMLRoot} from "hast";
+import {Image, Root} from "mdast";
+import {visit} from "unist-util-visit";
 
-export const OgImage: QuartzTransformerPlugin = (userOpts) => {
+export const OgImage: QuartzTransformerPlugin = () => {
     return {
-        name: "",
-        htmlPlugins() {
-            return []
+        name: "OgImagePlugin",
+        markdownPlugins() {
+            return [
+                () => {
+                    return async (tree: Root, file) => {
+                        const frontMatterImage = file.data.frontmatter?.image
+                        let firstImage : Image | undefined;
+                        visit(tree, "image", (node) => {
+                            if (!firstImage) {
+                                firstImage = node;
+                            }
+                        })
+                        file.data.image = frontMatterImage ?? firstImage?.url
+                    }
+                }
+            ]
         }
+    }
+}
+declare module "vfile" {
+    interface DataMap {
+        image: string
     }
 }
